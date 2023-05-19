955daf94cc2103e700f02445a8979224565def6b
christiancamilleri <camilleri.christian@outlook.com>
2023-05-19 16:08:20 +0200

    .

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -6,7 +6,7 @@ use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
-
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -27,7 +27,7 @@ class ProjectSeeder extends Seeder
             $project->link_repo = $faker->text(100);
             $project->languages = $faker->sentence(3);
             $project->frameworks = $faker->sentence(2);
-            $project->slug = $faker->sentence((2));
+            $project->slug = Str::slug($project->name, '-');
             $project->save();
         }
     }
